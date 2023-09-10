655a2d220cacf7f270924f0b214f5ce9530c68b6
Amir Seken <sekenamir@gmail.com>
2023-09-10 18:39:39 -0400

    added file.py code

---
+++ utils_tests.py
@@ -0,0 +1,17 @@
+from file import utils
+
+#integers
+print('integer inputs')
+print(utils.reversed(123))
+print(utils.formatter(5))
+
+#strings
+print('string inputs')
+print(utils.reversed('123'))
+print(utils.formatter('5'))
+
+#floats
+print('float inputs')
+print(utils.reversed(123.5))
+print(utils.formatter(5.5))
+
